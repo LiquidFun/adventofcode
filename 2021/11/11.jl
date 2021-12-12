4245535aa51c9ebd6c8d1e967f99ac58033f310c
LiquidFun <brutenis@gmail.com>
2021-12-12 20:45:17 +0100

    Add day 11

--- 2021/11/11.jl
+++ 2021/11/11.jl
@@ -1,21 +1,17 @@
 function main() 
-    lines = readlines()
-    arr = [parse(Int, c) for line=lines for c=line]
-    arr = reshape(arr, length(lines), length(lines[1]))
+    arr = collect.(readlines())
 
-    sy, sx = size(arr)
-    function is_valid(y, x)
-        return 1 <= y <= sy && 1 <= x <= sx
-    end
+    sy, sx = length(arr), length(arr[1])
+    is_valid(y, x) = 1 <= y <= sy && 1 <= x <= sx
 
     flashes = 0
     function flash(y, x)
-        if arr[y, x] > 9
+        if arr[y][x] > '9'
             flashes += 1
-            arr[y, x] = 0
-            for (y2, x2) in [(y+a, x+b) for a=-1:1, b=-1:1 if a!=0 || b!=0]
-                if is_valid(y2, x2) && arr[y2, x2] != 0
-                    arr[y2, x2] += 1
+            arr[y][x] = '0'
+            for (y2, x2) in [(y+a, x+b) for a=-1:1, b=-1:1]
+                if is_valid(y2, x2) && arr[y2][x2] != '0'
+                    arr[y2][x2] += 1
                     flash(y2, x2)
                 end
             end
@@ -25,20 +21,13 @@ function main()
     total_flashes = 0
     for step in 1:1000
         flashes = 0
-        arr .+= 1
-        for y in 1:sy
-            for x in 1:sx
-                flash(y, x)
-            end
+        (a -> a .+= 1).(arr)
+        for y in 1:sy, x in 1:sx
+            flash(y, x)
         end
         total_flashes += flashes
-        if step == 100
-            println(total_flashes)
-        end
-        if flashes == sy * sx
-            println(step)
-            break
-        end
+        step == 100 && println(total_flashes)
+        flashes == sy * sx && return println(step)
     end
 end
 
