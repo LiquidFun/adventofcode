14a75e458538b61b846d41b7349600b1f3f2cc65
LiquidFun <brutenis@gmail.com>
2021-12-15 00:43:17 +0100

    Add day 14

--- 2021/14/14.jl
+++ 2021/14/14.jl
@@ -1,19 +1,16 @@
-dp = Dict()
-lookup = Dict()
-function search_dp(count, letters)
-    (count, letters) in keys(dp) && return dp[(count, letters)]
-    left, right = letters[1] * lookup[letters], lookup[letters] * letters[2]
-    return dp[(count, letters)] = search_dp(count - 1, left) + search_dp(count - 1, right)
+dp, lookup = Dict(), Dict()
+function search_dp(key)
+    key in keys(dp) && return dp[key]
+    return dp[key] = sum(search_dp((key[1] - 1, letters)) for letters in lookup[key[2]])
 end
-first = readline()
-for line in filter(x -> occursin("->", x), readlines())
-    from, to = split(line, " -> ")
-    dp[(1, from)] = [sum(c .== collect(from[1] * to)) for c in 'A':'Z']
-    lookup[from] = to
+template = readline(); readline()
+for (from, to) in readlines() .|>  line -> split(line, " -> ")
+    dp[(1, from)] = [count(==(c), from[1] * to) for c in 'A':'Z']
+    lookup[from] = (from[1] * to, to * from[2])
 end
 function solve(for_step)
-    occ = sum(search_dp(for_step, a * b) for (a, b) in zip(first, first[2:end]))
-    occ[first[end] - 'A' + 1] += 1
+    occ = sum(search_dp((for_step, a * b)) for (a, b) in zip(template, template[2:end]))
+    occ[template[end] - 'A' + 1] += 1
     return maximum(occ) - minimum(filter(!=(0), occ))
 end
 println(solve(10))
