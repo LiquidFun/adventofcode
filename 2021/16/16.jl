27fe57ead7e01f32d335fea5fef22bcc85b5f60d
LiquidFun <brutenis@gmail.com>
2021-12-17 00:27:27 +0100

    Add day 16

--- 2021/16/16.jl
+++ 2021/16/16.jl
@@ -1,51 +1,14 @@
-
-function main()
-    codes = parse.(Int, collect(readline()), base=16)
-    bits = join(hcat(reverse.(digits.(codes, base=2, pad=4))...))
-    version_sum = 0
-    function parse_packet(s)
-        take_bits(count) = bits[s:((s+=count)-1)]
-        take_int(count) = parse(Int, take_bits(count), base=2)
-        version = take_int(3)
-        id = take_int(3)
-        version_sum += version
-        value = 0
-        if id == 4
-            bin_num = ""
-            while true
-                is_last_block = take_int(1) == 0
-                bin_num *= take_bits(4)
-                is_last_block && break
-            end
-            value = parse(Int, bin_num, base=2)
-        else
-            length_type_id = take_int(1)
-            length_data = take_int(length_type_id == 1 ? 11 : 15)
-            values = []
-            if length_type_id == 0
-                target_s = s + length_data
-                while s < target_s
-                    s, val = parse_packet(s)
-                    push!(values, val)
-                end
-            else
-                for packet in 1:length_data
-                    s, val = parse_packet(s)
-                    push!(values, val)
-                end
-            end
-            id == 0 && (value = sum(values))
-            id == 1 && (value = prod(values))
-            id == 2 && (value = minimum(values))
-            id == 3 && (value = maximum(values))
-            id == 5 && (value = values[1] > values[2])
-            id == 6 && (value = values[1] < values[2])
-            id == 7 && (value = values[1] == values[2])
-        end
-        return s, value
+version_sum, bits = 0, join(reverse(digits(parse(BigInt, "1"*readline(), base=16), base=2))[2:end])
+function parse_packet(index)  # index is the current index in `bits` string, list of single integer
+    take_int(count) = parse(Int, bits[index[1]:((index[1]+=count)-1)], base=2)
+    global version_sum += take_int(3)
+    id, c = take_int(3), true
+    id == 4 && return reduce((a,b)->16a+b, [take_int(4) for _=1:99 if c == 1 && (c = take_int(1)) < 2])
+    apply_op(values) = reduce([+, *, min, max, id, >, <, ==][id + 1], values)
+    if take_int(1) == 0 && (target_index = take_int(15) + index[1]) != 0
+        return [parse_packet(index) for _=1:99 if index[1] < target_index] |> apply_op
+    else
+        return [parse_packet(index) for _=1:take_int(11)] |> apply_op
     end
-    s, val = parse_packet(1)
-    println(version_sum)
-    println(val)
 end
-main()
+parse_packet([1]) |> val -> println("$version_sum\n$val") # |> so that sum can be printed before val
