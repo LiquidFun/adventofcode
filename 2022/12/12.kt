cef459bdc10c4cbc812ae7859a97c7684fd47142
LiquidFun <brutenis@gmail.com>
2022-12-12 22:54:30 +0100

    Add day 12

--- 2022/12/12.kt
+++ 2022/12/12.kt
@@ -1,15 +1,13 @@
 fun bfs(field: List<String>, queue: MutableList<Pair<Int, Int>>, target: Pair<Int, Int>): Int {
-    val dist: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
-    queue.forEach { dist[it] = 0 }
+    val dist = queue.associate { it to 0 }.toMutableMap()
     val dirs = listOf(-1 to 0, 1 to 0, 0 to 1, 0 to -1)
-    fun isValid(y: Int, x: Int) = 0 <= y && y < field.size && 0 <= x && x < field[0].length
     while (!queue.isEmpty()) {
         val (y, x) = queue.removeFirst()
         dirs
             .map { (ya, xa) -> Pair(y+ya, x+xa) }
-            .filter { isValid(it.first, it.second) }
+            .filter { (ya, xa) -> ya in field.indices && xa in field[0].indices }
             .filter { !dist.contains(it) }
-            .filter { (ya, xa) -> field[y][x] + 1 >= field[ya][xa] }
+            .filter { (ya, xa) -> field[ya][xa] <= field[y][x] + 1 }
             .forEach { 
                 dist[it] = dist[Pair(y, x)]!! + 1
                 queue.add(it)
@@ -23,7 +21,7 @@ fun main() {
     fun findChars(c: Char) = field
         .mapIndexed { y, l -> l.mapIndexed { x, _ -> y to x } }
         .flatten()
-        .filter { field[it.first][it.second] == c }
+        .filter { (y, x) -> field[y][x] == c }
         .toMutableList()
     val S = findChars('S')
     val E = findChars('E').first()
