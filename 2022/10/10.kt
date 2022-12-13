cc78ac47bcad59de5934f91e99bdc7a6ea55b6d6
LiquidFun <brutenis@gmail.com>
2022-12-13 01:07:04 +0100

    Add day 10

--- 2022/10/10.kt
+++ 2022/10/10.kt
@@ -1,8 +1,8 @@
 fun main() {
-    val vals = generateSequence(::readlnOrNull).joinToString("\n").replace(" ", "\n").split("\n")
-        .map { if (it.length == 4) 0 else it.toInt() }
-        .scan(1) {s, e -> s + e}
-        .withIndex()
+    val vals = generateSequence(::readlnOrNull).joinToString("\n").split(" ", "\n")
+         .map { it.toIntOrNull() ?: 0 }
+         .scan(1, Int::plus)
+         .withIndex()
     println(vals.filter { (it.index+1) % 40 == 20 }.sumOf { (it.index+1) * it.value })
     val p2 = vals
         .map { (i, v) -> if (v-1 <= i % 40 && i % 40 <= v+1) "#" else " " }
