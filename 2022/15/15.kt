ef017b9a7c02ca45cbd0a75108eab84563fb1f5f
LiquidFun <brutenis@gmail.com>
2022-12-16 00:45:38 +0100

    Add day 15

--- 2022/15/15.kt
+++ 2022/15/15.kt
@@ -1,21 +1,21 @@
+fun dist(x1: Long, y1: Long, x2: Long, y2: Long) = Math.abs(x1 - x2) + Math.abs(y1 - y2)
+
 fun main() {
     val sensors = generateSequence(::readlnOrNull).toList()
         .map { Regex("-?\\d+").findAll(it).toList() }
         .map { it.map { it.value.toLong() } }
-        .sortedWith() { a: List<Long>, b: List<Long> -> a[0].compareTo(b[0]) }
-    fun dist(x1: Long, y1: Long, x2: Long, y2: Long) = Math.abs(x1 - x2) + Math.abs(y1 - y2)
+        .sortedBy() { it[0] }
     val y = 2_000_000L
-    (-1_000_000L..6_000_000L).map { x -> 
+    (-1_000_000L..6_000_000L).count { x -> 
         sensors
-            .filter { (_, _, xb, yb) -> xb != x || yb != y }
-            .map { (xs, ys, xb, yb) -> dist(xs, ys, xb, yb) >= dist(xs, ys, x, y) }
-            .any { it }
-    }.count { it }.run(::println)
+            .filter { it[2] != x || it[3] != y }
+            .any { (xs, ys, xb, yb) -> dist(xs, ys, x, y) <= dist(xs, ys, xb, yb) }
+    }.run(::println)
 
     for (y in 0L..4_000_000L) {
         var x = 0L
         for ((xs, ys, xb, yb) in sensors)
-            if (dist(xs, ys, xb, yb) >= dist(xs, ys, x, y))
+            if (dist(xs, ys, x, y) <= dist(xs, ys, xb, yb))
                 x = xs + dist(xs, ys, xb, yb) - Math.abs(ys - y) + 1
         if (x <= 4_000_000L)
             println(x * 4_000_000L + y)
