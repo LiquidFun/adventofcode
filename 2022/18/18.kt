a2edd494e690be25b8277dba88f8b0af38229ae4
LiquidFun <brutenis@gmail.com>
2022-12-18 07:56:02 +0100

    Add day 18

--- 2022/18/18.kt
+++ 2022/18/18.kt
@@ -1,6 +1,8 @@
 data class Point(var x: Int, var y: Int, var z: Int) {
 	operator fun plus(o: Point): Point = Point(x+o.x, y+o.y, z+o.z)
 	fun adjacent(): List<Point> = dirs.map { this+it }.toList()
+	fun min(): Int = minOf(x, y, z)
+	fun max(): Int = maxOf(x, y, z)
 }
 
 val dirs = listOf(
@@ -12,26 +14,24 @@ fun main() {
 	val input = generateSequence(::readlnOrNull)
 		.map { it.split(",").map { it.toInt() } }
 		.map { (x, y, z) -> Point(x, y, z) }.toList()
-	val limitsX = input.minOf { it.x-1 }..input.maxOf { it.x+1 }
-	val limitsY = input.minOf { it.y-1 }..input.maxOf { it.y+1 }
-	val limitsZ = input.minOf { it.z-1 }..input.maxOf { it.z+1 }
+	val bounds = input.minOf { it.min()-1 }..input.maxOf { it.max()+1 }
 	
 	// Part 1
 	input.sumOf { it.adjacent().count { !input.contains(it) } }.run(::println)
 
 	// Part 2
-	val queue = ArrayDeque<Point>()
-	queue.add(Point(limitsX.first, limitsY.first, limitsZ.first))
-	var count = 0
+	val queue = mutableListOf(Point(bounds.first, bounds.first, bounds.first))
 	val visited: MutableSet<Point> = mutableSetOf()
+	var count = 0
+
 	while (queue.isNotEmpty()) {
-		val point = queue.removeFirst()
+		val point = queue.removeAt(0)
 		if (point in visited) continue
 		visited.add(point)
 		for (adj in point.adjacent()) {
 			if (adj in input) 
 				count++
-			else if (adj.x in limitsX && adj.y in limitsY && adj.z in limitsZ)
+			else if (adj.x in bounds && adj.y in bounds && adj.z in bounds)
 				queue.add(adj)
 		}
 	}
