95486e76bf04c2f509975277832a8cdb508ad249
LiquidFun <brutenis@gmail.com>
2023-12-02 08:10:00 +0100

    Add day 2

--- 2023/02/02.py
+++ 2023/02/02.py
@@ -1,12 +1,8 @@
+import re
 from sys import stdin
 s1, s2 = 0, 0
 for i, line in enumerate(stdin.readlines(), 1):
-    _, subsets = line.split(":")
-    r, g, b = 0, 0, 0
-    for subset in subsets.split(';'):
-        for showing in subset.split(","):
-            num, color = showing.strip().split()
-            globals()[color[0]] = max(globals()[color[0]], int(num))
+    r, g, b = [max(map(int, re.findall(fr"(\d+) {c}", line))) for c in "rgb"]
     s1 += (r <= 12 and g <= 13 and b <= 14) * i
     s2 += r * g * b
 
