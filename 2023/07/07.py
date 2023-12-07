35083b5619ebaa444c8592dd1c7b4ada84a74c3d
LiquidFun <brutenis@gmail.com>
2023-12-07 14:38:33 +0100

    Add day 7

--- 2023/07/07.py
+++ 2023/07/07.py
@@ -1,38 +1,22 @@
 from sys import stdin
 from collections import Counter
-from functools import partial
 
-def lookup(card, j_is_joker=False):
-    order = 'J23456789TQKA' if j_is_joker else '23456789TJQKA'
-    return order.index(card)
+def card_strength(cards, joker=''):
+    return [f'{joker}23456789TJQKA'.index(card) for card in cards]
 
-def hand_type(occ):
-    match sorted(occ.values(), reverse=True):
-        case [5]: return 6
-        case [4, _]: return 5
-        case [3, 2]: return 4
-        case [3, *_]: return 3
-        case [2, 2, _]: return 2
-        case [2, *_]: return 1
-        case [*_]: return 0
+def sort_by(cards_and_bid, joker=''):
+    types = []
+    for replace_j_with in ("23456789TQKA" if joker else 'J'):
+        cards = cards_and_bid[0].replace('J', replace_j_with)
+        types += [[c[1] for c in Counter(cards).most_common()]]
 
-def sort_by(cards_and_bid, j_is_joker=False):
-    best = 0
-    for j_replace in ("AKQT98765432" if j_is_joker else "J"):
-        cards = cards_and_bid[0].replace('J', j_replace)
-        best = max(best, hand_type(Counter(cards)))
+    return max(types), card_strength(cards_and_bid[0], joker)
 
-    raw_strength = [lookup(card, j_is_joker) for card in cards_and_bid[0]]
-    return best, raw_strength
-
-hands = []
-for line in stdin.read().strip().split("\n"):
-    hands.append(line.split())
-
-def solve(j_is_joker=False):
-    as_sorted = sorted(hands, key=partial(sort_by, j_is_joker=j_is_joker))
+def solve(joker=''):
+    as_sorted = sorted(hands, key=lambda k: sort_by(k, joker))
     return sum(i * int(bid) for i, (cards, bid) in enumerate(as_sorted, 1))
 
-print(solve(j_is_joker=False))
-print(solve(j_is_joker=True))
+hands = [line.split() for line in stdin.read().strip().split("\n")]
 
+print(solve())
+print(solve(joker='J'))
