7242b8bbcc6d63dd81d36ceaa5a0fac5e2cc5826
LiquidFun <brutenis@gmail.com>
2023-12-18 19:01:50 +0100

    Add day 18

--- 2023/18/18.py
+++ 2023/18/18.py
@@ -1,16 +1,13 @@
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-def solve(instructions, s=0):
-    ys, xs = [0], [0]
+def solve(instructions, s=0, y=0, x=0):
     for d, num in instructions:
-        s += num
-        ys.append(ys[-1] + num * dirs[d][0])
-        xs.append(xs[-1] + num * dirs[d][1])
-
-    for i in range(len(ys)):  # Shoelace formula
-        s += ys[i] * xs[i-1] - xs[i] * ys[i-1]
+        y2 = y + num * dirs[d][0]
+        x2 = x + num * dirs[d][1]
+        s += num + (y2*x - x2*y)  # Border + Shoelace formula
+        y, x = y2, x2
     print(s // 2 + 1)
 
 split = [line.split() for line in open(0)]
-solve([("RDLU".index(d), int(num)) for d, num, _ in split])
-solve([(int(c[-2]), int(c[2:-2], 16)) for *_, c in split])
+solve(("RDLU".index(d), int(num)) for d, num, _ in split)
+solve((int(c[7]), int(c[2:7], 16)) for *_, c in split)
