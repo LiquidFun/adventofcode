61f33267778f3d73e05f5b29eb692753c922a82e
LiquidFun <brutenis@gmail.com>
2023-12-18 15:38:39 +0100

    Add day 18

--- 2023/18/18.py
+++ 2023/18/18.py
@@ -1,7 +1,6 @@
 from sys import stdin
 
-lines = stdin.read().splitlines()
-dirs = {"R": (0, 1), "D": (1, 0), "L": (0, -1), "U": (-1, 0)}
+dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 def solve(instructions, s=0):
     ys, xs = [0], [0]
@@ -10,11 +9,10 @@ def solve(instructions, s=0):
         ys.append(ys[-1] + num * dirs[d][0])
         xs.append(xs[-1] + num * dirs[d][1])
 
-    # Polygon area: Shoelace algorithm
-    for i in range(len(ys)-1, -1, -1):
+    for i in range(len(ys)):  # Shoelace formula
         s += ys[i] * xs[i-1] - xs[i] * ys[i-1]
-    return s // 2 + 1
+    print(s // 2 + 1)
 
-split = [line.split() for line in lines]
-print(solve([(d, int(num)) for d, num, _ in split]))
-print(solve([("RDLU"[int(c[-2])], int(c[2:-2], 16)) for *_, c in split]))
+split = [line.split() for line in stdin]
+solve([("RDLU".index(d), int(num)) for d, num, _ in split])
+solve([(int(c[-2]), int(c[2:-2], 16)) for *_, c in split])
