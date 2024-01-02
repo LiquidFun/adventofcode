f74fdae019a2ae8ed474abb8c31e3f33685452a0
LiquidFun <brutenis@gmail.com>
2024-01-02 03:37:48 +0200

    Add day 2

--- 2023/02/02.py
+++ 2023/02/02.py
@@ -1,7 +1,6 @@
 import re
-from sys import stdin
 s1, s2 = 0, 0
-for i, line in enumerate(stdin.readlines(), 1):
+for i, line in enumerate(open(0), 1):
     r, g, b = [max(map(int, re.findall(fr"(\d+) {c}", line))) for c in "rgb"]
     s1 += (r <= 12 and g <= 13 and b <= 14) * i
     s2 += r * g * b
