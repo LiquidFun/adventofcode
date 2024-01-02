bbbb7044eb0d33b91bd6ab43fa719d0cf0a512c5
LiquidFun <brutenis@gmail.com>
2024-01-02 03:34:46 +0200

    Add day 1

--- 2023/01/01.py
+++ 2023/01/01.py
@@ -1,13 +1,11 @@
 import re
-from sys import stdin
 s1, s2 = 0, 0
-digits1 = list("0123456789")
-digits2 = "zero one two three four five six seven eight nine".split()
-as_num  = lambda num: digits2.index(num) if num in digits2 else int(num)
+digits = "0|1|2|3|4|5|6|7|8|9|_|one|two|three|four|five|six|seven|eight|nine"
+as_num = lambda num: digits.split('|').index(num) % 10
 
-for line in stdin.readlines():
-    nums1 = [n[1] for n in re.finditer('(?=(' + '|'.join(digits1) + '))', line)]
-    nums2 = [n[1] for n in re.finditer('(?=(' + '|'.join(digits1 + digits2) + '))', line)]
-    s1 += as_num(nums1[0]) * 10 + as_num(nums1[-1]) if nums1 else 0
-    s2 += as_num(nums2[0]) * 10 + as_num(nums2[-1]) if nums2 else 0
+for line in open(0):
+    nums1 = [as_num(n[1]) for n in re.finditer(f'(?=({digits[:20]}))', line) if n[1]]
+    nums2 = [as_num(n[1]) for n in re.finditer(f'(?=({digits}))', line) if n[1]]
+    s1 += nums1[0]*10 + nums1[-1] if nums1 else 0
+    s2 += nums2[0]*10 + nums2[-1] if nums2 else 0
 print(s1, s2, sep="\n")
