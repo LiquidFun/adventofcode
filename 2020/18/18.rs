b1b082766f58c8b676f898e6c94326b18f53cba6
LiquidFun <brutenis@gmail.com>
2024-08-28 23:39:41 +0200

    Add day 18

--- 2020/18/18.rs
+++ 2020/18/18.rs
@@ -1,45 +1,50 @@
-use std::io::stdin;
+use std::{collections::HashMap, io::stdin};
 
-fn eval(line: &str) -> i64 {
-    let mut stack = vec![0, -1];
-    eprintln!("line = {:?}", line);
-    for c in line.chars() {
-        let mut eval = false;
-        eprintln!("c = {:?}, stack = {:?}", c, stack);
+use itertools::Itertools;
+
+fn eval(line: &str, plus_precedence: i32) -> i64 {
+    let mut nums = Vec::new();
+    let mut operators = Vec::new();
+    let precedence = HashMap::from([('(', 10), ('+', plus_precedence), ('*', 1)]);
+    // eprintln!("line = {:?}", line);
+    for c in line.chars().chain(")".chars()) {
+        // eprintln!("c = {:?}, nums = {:?}, ops = {:?}", c, nums, operators);
         match c {
-            '0'..='9' => {
-                stack.push(c.to_digit(10).unwrap() as i64);
-                eval = true;
-            }
-            '(' => {
-                stack.push(0);
-                stack.push(-1);
-            }
+            '0'..='9' => nums.push(c.to_digit(10).unwrap() as i64),
+            '(' =>  operators.push(c),
             ')' => {
-                eval = true;
+                while !operators.is_empty() {
+                    let op = operators.pop().unwrap();
+                    if op == '(' {
+                        break;
+                    }
+                    let func = if op == '*' { |(a, b)| a * b } else { |(a, b)| a + b };
+                    let value = nums.pop().zip(nums.pop()).map(func).unwrap();
+                    nums.push(value);
+                }
+            }
+            '+' | '*' => {
+                while !operators.is_empty() {
+                    // eprintln!("\tc = {:?}, nums = {:?}, ops = {:?}", c, nums, operators);
+                    let op = operators.last().unwrap();
+                    if *op == '(' || precedence.get(op).unwrap() < precedence.get(&c).unwrap() {
+                        break;
+                    }
+                    let func = if *op == '*' { |(a, b)| a * b } else { |(a, b)| a + b };
+                    let value = nums.pop().zip(nums.pop()).map(func).unwrap();
+                    nums.push(value);
+                    operators.pop();
+                }
+                operators.push(c);
             },
-            '+' => stack.push(-1),
-            '*' => stack.push(-2),
             _ => (),
         }
-        if eval {
-            let digit1 = stack.pop().unwrap();
-            let op = stack.pop().unwrap();
-            let digit2 = stack.pop().unwrap();
-            match op {
-                -1 => stack.push(digit2 + digit1),
-                _ => stack.push(digit2 * digit1),
-            }
-        }
     }
-    *stack.get(0).unwrap()
+    *nums.get(0).unwrap()
 }
 
 fn main() {
-    let out = stdin()
-        .lines()
-        .filter_map(Result::ok)
-        .map(|line| eval(&line))
-        .sum::<i64>();
-    println!("{:?}", out)
+    let lines = stdin() .lines().filter_map(Result::ok).collect_vec();
+    println!("{:?}", lines.iter().map(|l| eval(&l, 1)).sum::<i64>()); 
+    println!("{:?}", lines.iter().map(|l| eval(&l, 2)).sum::<i64>()); 
 }
