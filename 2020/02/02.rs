a755cfecf86f3ee94bf54c76bafc528ab0b73e1a
LiquidFun <brutenis@gmail.com>
2024-09-07 20:46:01 +0200

    Add day 2

--- 2020/02/02.rs
+++ 2020/02/02.rs
@@ -1,45 +1,38 @@
-use regex::Regex;
-use std::io::Read;
+use itertools::Itertools;
+use std::io::stdin;
 
-#[derive(Debug)]
 struct PasswordPhilosophy {
-    range: std::ops::Range<i32>,
+    range: std::ops::RangeInclusive<usize>,
     letter: char,
     string: String,
 }
 
 fn get_password_philosophies() -> Vec<PasswordPhilosophy> {
-    let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input).unwrap();
-    let pattern = Regex::new(r"(\d+)-(\d+) (\w): (\w+)").unwrap();
-    let mut philosophies = Vec::new();
-    for cap in pattern.captures_iter(&input) {
-        let range = cap[1].parse::<i32>().unwrap()..cap[2].parse::<i32>().unwrap()+1;
-        let letter = cap[3].chars().next().unwrap();
-        philosophies.push(PasswordPhilosophy { range, letter, string: cap[4].to_string() });
-    }
-    philosophies
+    let lines = stdin().lines().filter_map(Result::ok).collect_vec();
+    lines.iter()
+        .map(|line| line.split(|c| "- :".contains(c)).next_tuple().unwrap())
+        .map(|(a, b, letter, _, string)| {
+            PasswordPhilosophy {
+                range: a.parse().unwrap()..=b.parse().unwrap(),
+                letter: letter.chars().next().unwrap(),
+                string: string.to_owned(),
+            }
+        })
+        .collect()
 }
-
 
 fn main() {
     let philosophies = get_password_philosophies();
-    let mut sum1 = 0;
-    for p in &philosophies {
-        let letter_count = p.string.matches(p.letter).count();
-        if p.range.contains(&(letter_count as i32)) {
-            sum1 += 1
-        }
-    }
+    println!("{}", philosophies.iter().filter(|p| p.range.contains(&p.string.matches(p.letter).count())).count());
 
-    let mut sum2 = 0;
-    for p in &philosophies {
-        let is_first = p.string.chars().nth((p.range.start-1) as usize).unwrap() == p.letter;
-        let is_second = p.string.chars().nth((p.range.end-2) as usize).unwrap() == p.letter;
-        if is_first != is_second {
-            sum2 += 1
-        }
-    }
+    let sum2 = philosophies
+        .iter()
+        .filter(|p| {
+            let is_first = p.string.chars().nth((p.range.start() - 1) as usize).unwrap() == p.letter;
+            let is_second = p.string.chars().nth((p.range.end() - 1) as usize).unwrap() == p.letter;
+            is_first != is_second
+        })
+        .count();
 
-    println!("{}\n{}", sum1, sum2);
+    println!("{}", sum2);
 }
