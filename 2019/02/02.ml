469e59a0393c422b87cff9d8389179ab8e6102e4
LiquidFun <brutenis@gmail.com>
2024-09-15 23:07:51 +0200

    Add day 2

--- 2019/02/02.ml
+++ 2019/02/02.ml
@@ -4,16 +4,16 @@ let rec process nums i =
   | 2 -> nums.(nums.(i+3)) <- nums.(nums.(i+1)) * nums.(nums.(i+2)); process nums (i+4)
   | _ -> nums.(0)
   
-let with_a_b nums noun verb = 
+let with_a_b nums n = 
   let memory = Array.copy nums in
-  memory.(1) <- noun;
-  memory.(2) <- verb;
+  memory.(1) <- n / 100;
+  memory.(2) <- n mod 100;
   process memory 0
 
 let find_part2 nums =
-  List.init 10000 Fun.id |> List.find (fun x -> (with_a_b nums (x / 100) (x mod 100)) == 19690720)
+  List.init 10000 Fun.id |> List.find (fun x -> (with_a_b nums x) == 19690720)
 
 let () = 
   let nums = input_line stdin |> String.split_on_char ',' |> List.map int_of_string |> Array.of_list in
-  with_a_b nums 12 2 |> Printf.printf "%d\n";
+  with_a_b nums 1202 |> Printf.printf "%d\n";
   find_part2 nums |> print_int
