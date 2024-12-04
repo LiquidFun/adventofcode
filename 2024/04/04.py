f4dbe79a2b1227027efbc9cad31c442070b8df72
LiquidFun <brutenis@gmail.com>
2024-12-04 07:26:12 +0100

    Add day 4

--- 2024/04/04.py
+++ 2024/04/04.py
@@ -1,20 +1,11 @@
-X = [l.strip() + "   " for l in open(0).readlines()]
-X.extend([" " * len(X[0])] * 3)
+coords = {x+1j*y: c for y, r in enumerate(open(0)) for x, c in enumerate(r)}
+g = lambda c: coords.get(c, "")
+
 s1 = s2 = 0
-dirs = [
-    [(0, 0), (1, 1), (2, 2), (3, 3)],
-    [(0, 0), (1, 0), (2, 0), (3, 0)],
-    [(0, 0), (0, 1), (0, 2), (0, 3)],
-    [(3, 0), (2, 1), (1, 2), (0, 3)],
-]
-
-for y in range(len(X)-3):
-    for x in range(len(X[y])-3):
-        for dir in dirs:
-            s1 += ''.join(X[y+dy][x+dx] for dy, dx in dir) in ("XMAS", "SAMX")
-
-        a = (X[y][x] + X[y+2][x+2]) in ("MS", "SM")
-        b = (X[y+2][x] + X[y][x+2]) in ("MS", "SM")
-        s2 += X[y+1][x+1] == "A" and a and b
+for c in coords:
+    for d in [1, 1j, 1+1j, 1-1j, -1, -1j, -1+1j, -1-1j]:
+        s1 += g(c) + g(c+d) + g(c+d*2) + g(c+d*3) == "XMAS"
+        if d.imag and d.real:
+            s2 += g(c+d) + g(c) + g(c-d) == "MAS" and g(c+d*1j) + g(c-d*1j) == "MS"
 
 print(s1, s2, sep="\n")
