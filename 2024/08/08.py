97b641f7f31bd47f99538741a2261c9963dda79d
LiquidFun <brutenis@gmail.com>
2024-12-08 22:34:46 +0100

    Add day 8

--- 2024/08/08.py
+++ 2024/08/08.py
@@ -1,51 +1,18 @@
-from collections import *
 from itertools import *
-from functools import *
-import re
 
 coords = {x+1j*y: c for y, r in enumerate(open(0)) for x, c in enumerate(r.strip())}
 
-# d4 = [1, 1j, -1, -1j]
-# d8 = d4 + [1+1j, 1-1j, -1+1j, -1-1j]
-# def adjacent(coord, dirs=d4):
-#     return [coord + d for d in dirs]
+by = lambda v: v[1]
+order = groupby(sorted(coords.items(), key=by), by)
 
+p1, p2 = set(), set()
 
-order = defaultdict(list)
-
-for coord, char in coords.items():
-    if char != '.':
-        order[char].append(coord)
-
-antinodes = set()
-# print(order)
-
-for char, coord in order.items():
-    for a, b in combinations(coord, r=2):
+for char, coord in order:
+    if char == '.': continue
+    for (a, _), (b, _) in combinations(coord, r=2):
         diff = a - b
-        i = 0
-        while (new := a + diff * i) in coords:
-            antinodes.add(new)
-            i += 1 
-        i = 0
-        while (new := b - diff * i) in coords:
-            antinodes.add(new)
-            i += 1 
-# print()
-antinodes &= coords.keys()
-# print(antinodes)
+        p1 |= {a + diff*i for i in (-2, 1)}
+        p2 |= {a + diff*i for i in range(-100, 100)}
 
-for y in range(12):
-    for x in range(12):
-        c= y*1j+x
-         
-        print(end='#' if c in antinodes else coords[c])
-    print()
-    
-print(len(antinodes))
-
-
-
-# for line in open(0):
-#     n = [int(a) for a in line.split()]
-    # re.findall(r"\d+", line)
+print(len(p1 & coords.keys()))
+print(len(p2 & coords.keys()))
