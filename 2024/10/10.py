6a0fa6d715fd95e0ff352bc30af1c75655a5593d
LiquidFun <brutenis@gmail.com>
2024-12-10 13:27:22 +0100

    Add day 10

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -1,18 +1,18 @@
-coords = {x+1j*y: int(c) for y, r in enumerate(open(0)) for x, c in enumerate(r.strip())}
+coords = {x+1j*y: int(h) for y, r in enumerate(open(0)) for x, h in enumerate(r.strip())}
 
-def traverse(c, unique, some=0):
+def hike(c, peaks, paths=0):
     if coords[c] == 9: 
-        unique.add(c)
+        peaks.add(c)
         return 1
     for d in [1, 1j, -1, -1j]:
         if coords.get(c+d) == coords[c] + 1:
-            some += traverse(c+d, unique)
-    return some
+            paths += hike(c+d, peaks)
+    return paths
 
 p1, s2 = {c: set() for c in coords}, 0
 for c in coords:
     if coords[c] == 0:
-        s2 += traverse(c, p1[c])
+        s2 += hike(c, p1[c])
 
 print(sum(map(len, p1.values())))
 print(s2)
