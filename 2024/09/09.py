dfb4e261270064c135308f2dd51ee9ef5d2044bc
LiquidFun <brutenis@gmail.com>
2024-12-10 06:26:31 +0100

    Add day 9

--- 2024/09/09.py
+++ 2024/09/09.py
@@ -37,35 +37,3 @@ def solve2(files, free_spaces, s=0, index=-1):
 filesystem = [int(a) for a in input()]
 print(solve1(filesystem[:]))
 print(solve2(filesystem[::2], filesystem[1::2]))
-
-# print(spaces)
-
-
-# for file in files[::-1][::2]:
-
-# for i, file in enumerate(files):
-#     for _ in file:
-#         s += i * (index := index + 1)
-
-
-# while f+1 < len(files):
-#     file = files[f := f + 1]
-#     is_file = f%2 == 0
-#     for _ in range(file):
-#         i = f//2 if is_file else len(files) // 2
-#         print(end=str(i)+"'")
-#         print(f, i, is_file, len(files)//2, files[-3:])
-#         s += i * (index := index + 1)
-#         if not is_file:
-#             files[-1] -= 1
-#             while files[-1] == 0 or len(files) % 2 == 0 and len(files)-1 != f:
-#                 files.pop()
-#             if len(files)-1 == f:
-#                 break
-
-# print(s)
-# for f, file in enumerate(files):
-# not 8276976286384
-# not 6341974755416
-# not 6341974805198
-
