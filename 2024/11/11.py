854cd88b4996ddd1a01f01d9433fa31b06eedf81
LiquidFun <brutenis@gmail.com>
2024-12-11 16:02:20 +0100

    Add day 11

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -4,13 +4,11 @@ counts = Counter([int(a) for a in input().split()])
 
 for i in range(75):
     for n, occ in list(counts.items()):
-        if n == 0:
-            counts[1] += occ
-        elif len(s := str(n)) % 2 == 0:
-            counts[int(s[:len(s)//2])] += occ
-            counts[int(s[len(s)//2:])] += occ
+        if (l := len(str(n))) % 2 == 0:
+            counts[n // 10**(l//2)] += occ
+            counts[n % 10**(l//2)] += occ
         else:
-            counts[n * 2024] += occ
+            counts[n * 2024 or 1] += occ
         counts[n] -= occ
     if i in (24, 74):
         print(sum(counts.values()))
