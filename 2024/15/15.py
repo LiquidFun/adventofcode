fef6f270f03c5c15d9e4b8ec2811145307b410aa
LiquidFun <brutenis@gmail.com>
2024-12-15 15:52:35 +0200

    Add day 15

--- 2024/15/15.py
+++ 2024/15/15.py
@@ -1,34 +1,28 @@
-def solve(field, gps=0):
+def solve(field):
     coords = {x+1j*y: c for y, r in enumerate(field.split("\n")) for x, c in enumerate(r)}
 
-    s = [c for c, char in coords.items() if char == "@"][0]
-    coords[s] = "."
+    pos = [c for c in coords if coords[c] == "@"][0]
+    coords[pos] = "."
 
-    def collect(c):
-        if coords[c+d] not in "]O[":
+    def find_boxes(c):
+        if coords[c] not in "]O[":
             return {}
-        dn = d + ("]O[".index(coords[c+d]) - 1 if d.imag else 0) 
-        return {c+d: coords[c+d], c+dn: coords[c+dn]} \
-               | collect(c+d) \
-               | collect(c+dn)
+        c_adj = c + ("]O[".index(coords[c]) - 1 if dir.imag else 0)
+        return {c: coords[c], c_adj: coords[c_adj]} \
+               | find_boxes(c+dir) \
+               | find_boxes(c_adj+dir)
 
-    for m in moves.replace("\n", ""):
-        d = {'>': 1, 'v': 1j, '<': -1, '^': -1j}[m]
-        
-        if coords[s + d] != '#':
-            boxes = collect(s)
-            for x, char in boxes.items():
-                if coords[x+d] == '#':
-                    break
-            else:
-                for x, char in boxes.items():
-                    coords[x] = '.'
-                for x, char in boxes.items():
-                    coords[x+d] = char
-                s = s+d
-        
-    print(int(sum(x.imag * 100 + x.real for x, c in coords.items() if c in '[O')))
+    for move in moves.replace("\n", ""):
+        dir = {'>': 1, 'v': 1j, '<': -1, '^': -1j}[move]
+        if coords[pos+dir] == '#': continue
 
+        boxes = find_boxes(pos+dir)
+        if all(coords[box+dir] != "#" for box in boxes):
+            coords |= {box: '.' for box in boxes}
+            coords |= {box+dir: boxes[box] for box in boxes}
+            pos += dir
+        
+    print(int(sum(c.imag*100 + c.real for c in coords if coords[c] in '[O')))
 
 field, moves = open(0).read().split("\n\n")
 
