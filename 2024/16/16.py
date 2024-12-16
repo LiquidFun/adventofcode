fba11fadb49161f31a014f46a224ac7df8fdbd69
LiquidFun <brutenis@gmail.com>
2024-12-16 15:15:20 +0200

    Add day 16

--- 2024/16/16.py
+++ 2024/16/16.py
@@ -2,22 +2,16 @@ import networkx as nx
 coords = {x+1j*y: c for y, r in enumerate(open(0)) for x, c in enumerate(r.strip())}
 
 G = nx.DiGraph()
-
 for c in coords:
-    if coords[c] == '#': continue
     for d in [1, 1j, -1, -1j]:
-        G.add_edge((c, d), c, weight=0)
-        G.add_edge(c, (c, d), weight=1000)
-        if coords[c+d] != '#':
+        G.add_edge((c, d), (c, 0), weight=0)
+        G.add_edge((c, 0), (c, d), weight=1000)
+        if coords[c] != '#' != coords[c+d]:
             G.add_edge((c, d), (c+d, d), weight=1)
 
-S = [c for c in coords if coords[c] == 'S'][0]
-E = [c for c in coords if coords[c] == 'E'][0]
+E, S = [c for c in coords if coords[c] in 'SE']
 
-all_paths = list(nx.all_shortest_paths(G, (S, 1), E, "weight"))
-path = all_paths[0]
-print(sum(G.edges[edge]["weight"] for edge in zip(path, path[1:])))
+paths = list(nx.all_shortest_paths(G, (S, 1), (E, 0), "weight"))
 
-flat = sum(all_paths, [])
-nodes = {(p[0] if isinstance(p, tuple) else p) for p in flat}
-print(len(nodes))
+print(sum(G.edges[e]["weight"] for e in zip(paths[0], paths[0][1:])))
+print(len({p[0] for p in sum(paths, [])}))
