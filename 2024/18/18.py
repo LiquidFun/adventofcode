22473dba038a89b5831de0cf52f9f9f0938ffdc4
LiquidFun <brutenis@gmail.com>
2024-12-18 12:28:54 +0200

    Add day 18

--- 2024/18/18.py
+++ 2024/18/18.py
@@ -1,16 +1,13 @@
 import networkx as nx
 
 def solve(obstacles, S=70):
-    G = nx.DiGraph()
-    for c in {x+1j*y for y in range(S+1) for x in range(S+1)}:
-        for d in [1, 1j, -1, -1j]:
-            if c not in obstacles and c+d not in obstacles:
-                G.add_edge(c, c+d)
+    G = nx.grid_graph((S+1, S+1))
+    G.remove_nodes_from(obstacles)
+    return nx.has_path(G, (0,0), (S,S)) \
+           and len(nx.shortest_path(G, (0,0), (S,S)))
 
-    return nx.has_path(G, 0, S+S*1j) and len(nx.shortest_path(G, 0, S+S*1j))
-
-obstacles = [complex(line.replace(",", "+")+"j") for line in open(0)]
-print(solve(obstacles[:1024]))
+obstacles = [tuple(map(int, line.split(","))) for line in open(0)]
+print(solve(obstacles[:1024])-1)
 
 lo, hi = 1024, len(obstacles)-1
 while lo < hi:
@@ -18,4 +15,4 @@ while lo < hi:
     if solve(obstacles[:mid]): lo = mid+1
     else: hi = mid-1
 
-print(str(obstacles[mid]).replace("+", ",")[1:-2])
+print(*obstacles[mid], sep=",")
