b362d3afbc89ff7b8d21d96b5617baa0cbd6766c
LiquidFun <brutenis@gmail.com>
2024-12-21 16:27:15 +0200

    Add day 21

--- 2024/21/21.py
+++ 2024/21/21.py
@@ -1,5 +1,4 @@
 from functools import cache
-from random import random
 
 N = {'7':0, '8':1, '9':2, '4':1j, '5':1+1j, '6':2+1j, 
       '1':2j, '2':1+2j, '3':2+2j, ' ':3j, '0':1+3j, 'A':2+3j}
@@ -13,11 +12,9 @@ def path(start, end):
     yy = ("^"*-dy) + ("v"*dy)
     xx = ("<"*-dx) + (">"*dx)
 
-    if pad[start] + dy*1j == pad[" "]:
-        return xx + yy + "A"
-    if pad[start] + dx == pad[" "]:
-        return yy + xx + "A"
-    return (xx+yy if random() < 0.5 else yy+xx) + "A"  # ¯\_(ツ)_/¯
+    bad = pad[" "] - pad[start]
+    prefer_yy_first = (dx>0 or bad==dx) and bad!=dy*1j
+    return (yy+xx if prefer_yy_first else xx+yy) + "A"
     
 @cache
 def length(code, depth, s=0):
@@ -26,14 +23,6 @@ def length(code, depth, s=0):
         s += length(path(code[i-1], c), depth-1)
     return s
 
-def solve(code, max_depth):
-    path.cache_clear()
-    length.cache_clear()
-    return int(code[:-1]) * length(code, max_depth)
-
-def simulate(code, max_depth):
-    return min(solve(code, max_depth) for _ in range(1000))
-
 codes = open(0).read().split()
-print(sum(simulate(code, 3) for code in codes))
-print(sum(simulate(code, 26) for code in codes))
+print(sum(int(code[:-1]) * length(code, 3) for code in codes))
+print(sum(int(code[:-1]) * length(code, 26) for code in codes))
