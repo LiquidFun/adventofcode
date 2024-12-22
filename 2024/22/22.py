258364d79c5dd5565b523336f642ad6261b3de1a
LiquidFun <brutenis@gmail.com>
2024-12-22 13:47:05 +0200

    Add day 22

--- 2024/22/22.py
+++ 2024/22/22.py
@@ -1,22 +1,17 @@
-from collections import Counter
-
-s1 = [0]
-
-def secret(num):
-    prices = Counter()
-    last4 = (10,10,10,10)
-    for i in range(2000):
+s1, s2 = 0, {}
+for num in map(int, open(0)):
+    seen = set()
+    last4 = (10, 10, 10, 10)
+    for _ in range(2000):
         prev = num%10
         num ^= (num*64) % 16777216
-        num ^= (num//32) % 16777216
+        num ^= num//32
         num ^= (num*2048) % 16777216
         last4 = last4[1:] + (num%10 - prev,)
-        if last4 not in prices:
-            prices[last4] = num%10
-    s1[0] += num
-    return prices
+        if last4 not in seen:
+            seen.add(last4)
+            s2[last4] = s2.get(last4, 0) + num%10
+    s1 += num
 
-ints = map(int, open(0))
-c = sum(map(secret, ints), Counter())
-print(s1[0])
-print(sorted(c.values())[-1])
+print(s1)
+print(sorted(s2.values())[-1])
