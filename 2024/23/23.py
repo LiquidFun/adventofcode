c8551a094a7edc377dc0af3c829d99172dad4f74
LiquidFun <brutenis@gmail.com>
2024-12-23 08:05:18 +0200

    Add day 23

--- 2024/23/23.py
+++ 2024/23/23.py
@@ -3,6 +3,5 @@ import networkx as nx
 G = nx.Graph(line.strip().split("-") for line in open(0))
 cliques = list(nx.enumerate_all_cliques(G))
 
-has_t0 = lambda clique: any(a[0]=='t' for a in clique)
-print(sum(has_t0(c) for c in cliques if len(c) == 3))
+print(sum(any(a[0]=='t' for a in c) for c in cliques if len(c) == 3))
 print(','.join(sorted(cliques[-1])))
