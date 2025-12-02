323ece396070830fcf8561c808e91a4d96d49768
Brutenis Gliwa <LiquidFun@users.noreply.github.com>
2025-12-02 17:41:44 +0100

    Add day 2

--- 2025/02/02.js
+++ 2025/02/02.js
@@ -1,21 +1,14 @@
-let ranges = (require("fs").readFileSync(0)+"").trim().split(",")
-let s1 = s2 = 0;
-for (let range of ranges) {
-  let [a, b] = range.split("-")
-  for (let i = +a; i <= +b; i++) {
-    let s = i.toString()
-    let size = s.length
-    s1 += s.slice(0, size/2) == s.slice(size/2) ? +i : 0
-    for (let k = 1; k <= size/2; k++) {
-      let set = new Set();
-      for (let l = 0; l < size; l += k) {
-        set.add(s.slice(l, l+k))
-      }
-      if (set.size == 1) {
-        s2 += i;
-        break;
-      }
-    }
-  }
-}
-console.log(`${s1}\n${s2}`)
+const range = n => [...Array(n).keys()]
+const sum = arr => arr.reduce((a,b) => +a + +b);
+
+const nums = require("fs").readFileSync(0, "utf8").trim()
+  .split(",")
+  .map(line => line.split`-`.map(Number))
+  .flatMap(([a, b]) => range(b-a).map(i => i+a+""))
+
+const isEqual = s => s.slice(s.length/2).repeat(2) == s
+const isAnyEqual = s => range(s.length/2|0)
+  .some(i => s.slice(0, i+1).repeat(s.length/(i+1)) == s)
+
+console.log(sum(nums.filter(isEqual)))
+console.log(sum(nums.filter(isAnyEqual)))
