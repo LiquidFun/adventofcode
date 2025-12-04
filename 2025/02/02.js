1a92bbee184c718699c36e1568806fda068c0269
LiquidFun <brutenis@gmail.com>
2025-12-04 18:51:26 +0100

    Add day 2

--- 2025/02/02.js
+++ 2025/02/02.js
@@ -1,9 +1,7 @@
-const sum = arr => arr.reduce((a,b) => a + b)
-
 const nums = require("fs").readFileSync(0, "utf8").trim()
   .split(",")
-  .map(line => line.split`-`.map(Number))
-  .flatMap(([a, b]) => [...Array(b-a+1).keys()].map(i => i+a))
+  .map(l => l.split`-`.map(Number))
+  .flatMap(([a,b]) => [...Array(b-a+1).keys()].map(i => i+a))
 
-console.log(sum(nums.filter(s => /^(.*)\1$/.test(s))))
-console.log(sum(nums.filter(s => /^(.*)\1+$/.test(s))))
+console.log(nums.reduce((a,b) => a + b * /^(.*)\1$/.test(b), 0))
+console.log(nums.reduce((a,b) => a + b * /^(.*)\1+$/.test(b), 0))
