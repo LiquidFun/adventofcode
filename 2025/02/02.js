1361bbce79d65fffa597f09a59e433aecde64ea3
LiquidFun <brutenis@gmail.com>
2025-12-04 18:45:03 +0100

    Add day 2

--- 2025/02/02.js
+++ 2025/02/02.js
@@ -1,14 +1,9 @@
-const range = n => [...Array(n).keys()]
-const sum = arr => arr.reduce((a,b) => +a + +b);
+const sum = arr => arr.reduce((a,b) => a + b)
 
 const nums = require("fs").readFileSync(0, "utf8").trim()
   .split(",")
   .map(line => line.split`-`.map(Number))
-  .flatMap(([a, b]) => range(b-a+1).map(i => i+a+""))
+  .flatMap(([a, b]) => [...Array(b-a+1).keys()].map(i => i+a))
 
-const isEqual = s => s.slice(s.length/2).repeat(2) == s
-const isAnyEqual = s => range(s.length/2|0)
-  .some(i => s.slice(0, i+1).repeat(s.length/(i+1)) == s)
-
-console.log(sum(nums.filter(isEqual)))
-console.log(sum(nums.filter(isAnyEqual)))
+console.log(sum(nums.filter(s => /^(.*)\1$/.test(s))))
+console.log(sum(nums.filter(s => /^(.*)\1+$/.test(s))))
