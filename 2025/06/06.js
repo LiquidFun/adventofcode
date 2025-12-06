fc58026434e5777980867a84929bf913ee47468a
LiquidFun <brutenis@gmail.com>
2025-12-06 13:04:40 +0100

    Add day 6

--- 2025/06/06.js
+++ 2025/06/06.js
@@ -1,19 +1,19 @@
 Array.prototype.sumPrint = function () { console.log(this.reduce((a, b) => a + b)) }
 
 const lines = require("fs").readFileSync(0, "utf8").trimEnd().split("\n")
-const nums = lines.map(line => line.trim().split(/\s+/))
-const ops = nums.pop()
+const grid = lines.map(line => line.trim().split(/\s+/))
+const ops = grid.pop()
 lines.pop()
 
-const transpose = arr => [...arr[0]].map((_, i) => arr.map(n => n[i]))
+const transpose = arr => [...arr[0]].map((_, i) => arr.map(row => row[i]))
 
-transpose(nums)
-  .map((n, i) => eval(n.join(ops[i])))
+transpose(grid)
+  .map((row, i) => eval(row.join(ops[i])))
   .sumPrint()
 
 transpose(lines)
-  .map(l => +l.join(""))
+  .map(row => +row.join(""))
   .join()
   .split(",0,")
-  .map((n, i) => eval(n.replaceAll(",", ops[i])))
+  .map((row, i) => eval(row.replaceAll(",", ops[i])))
   .sumPrint()
