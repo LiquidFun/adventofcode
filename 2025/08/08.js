929a0ec9ff8a3a3478f6f028dad4b3f03d7c9883
LiquidFun <brutenis@gmail.com>
2025-12-09 00:29:34 +0100

    Add day 8

--- 2025/08/08.js
+++ 2025/08/08.js
@@ -3,34 +3,27 @@ let coords = require("fs").readFileSync(0, "utf8").trim().split("\n")
 
 let dist = ([[x1,y1,z1], [x2,y2,z2]]) => Math.hypot(x1-x2, y1-y2, z1-z2)
 
-let deltas = coords
+let sortedPairs = coords
   .flatMap((c1, i) => coords.slice(i+1).map(c2 => [c1, c2]))
   .sort((a, b) => dist(a) - dist(b))
   .map(x => x.map(y => coords.indexOf(y)))
 
-let adj = Array(1000).fill().map(_ => [])
+let unions = [...Array(1000).keys()].map(i => new Set([i]))
 
-for (let [k, [i, j]] of deltas.entries()) {
-  adj[i].push(j)
-  adj[j].push(i)
-
-  let visited = new Set()
-
-  function floodFill(i) {
-    if (visited.has(i)) return 0
-    visited.add(i)
-    return 1 + adj[i].map(floodFill).reduce((a,b) => a+b, 0)
-  }
+for (let [k, [i, j]] of sortedPairs.entries()) {
+  let old = unions[j]
+  old.forEach(a => unions[i].add(a))
+  unions = unions.map(u => u === old ? unions[i] : u)
 
   if (k == 1000) {
     console.log(
-      [...Array(1000).keys()]
-        .map(floodFill)
+      [...new Set(unions)]
+        .map(u => u.size)
         .sort((a,b) => a-b)
         .slice(-3)
         .reduce((a,b) => a*b))
   }
-  if (floodFill(0) == 1000) {
+  if (unions[0].size == 1000) {
     console.log(coords[i][0] * coords[j][0])
     break
   }
