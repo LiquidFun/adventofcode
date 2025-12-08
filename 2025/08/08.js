4d6cfd4ed3d879dc00ddc30e21cf6bacf97e8aeb
LiquidFun <brutenis@gmail.com>
2025-12-08 23:53:02 +0100

    Add day 8

--- 2025/08/08.js
+++ 2025/08/08.js
@@ -1,20 +1,16 @@
 let coords = require("fs").readFileSync(0, "utf8").trim().split("\n")
   .map(line => line.split(",").map(Number))
 
-let deltas = []
-for (let i = 0; i < coords.length; i++) {
-  for (let j = i+1; j < coords.length; j++) {
-    let [x1,y1,z1] = coords[i]
-    let [x2,y2,z2] = coords[j]
-    let delta = Math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
-    deltas.push([delta, i, j])
-  }
-}
+let dist = ([[x1,y1,z1], [x2,y2,z2]]) => Math.hypot(x1-x2, y1-y2, z1-z2)
 
-deltas.sort(([d1, i1, j1], [d2, i2, j2]) => d1 - d2)
+let deltas = coords
+  .flatMap((c1, i) => coords.slice(i+1).map(c2 => [c1, c2]))
+  .sort((a, b) => dist(a) - dist(b))
+  .map(x => x.map(y => coords.indexOf(y)))
+
 let adj = Array(1000).fill().map(_ => [])
 
-for (let [k, [_, i, j]] of deltas.entries()) {
+for (let [k, [i, j]] of deltas.entries()) {
   adj[i].push(j)
   adj[j].push(i)
 
