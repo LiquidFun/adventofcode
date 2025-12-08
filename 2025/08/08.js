d6c8cb4eebe8896dc87fe699a4d5096688594727
LiquidFun <brutenis@gmail.com>
2025-12-09 00:53:29 +0100

    Add day 8

--- 2025/08/08.js
+++ 2025/08/08.js
@@ -5,26 +5,23 @@ let dist = ([[x1,y1,z1], [x2,y2,z2]]) => Math.hypot(x1-x2, y1-y2, z1-z2)
 
 let sortedPairs = coords
   .flatMap((c1, i) => coords.slice(i+1).map(c2 => [c1, c2]))
-  .sort((a, b) => dist(a) - dist(b))
-  .map(x => x.map(y => coords.indexOf(y)))
+  .sort((c1, c2) => dist(c1) - dist(c2))
+  .map(pair => pair.map(c => coords.indexOf(c)))
 
-let unions = [...Array(1000).keys()].map(i => new Set([i]))
+let unions = coords.map((_, i) => new Set([i]))
 
-for (let [k, [i, j]] of sortedPairs.entries()) {
-  let old = unions[j]
-  old.forEach(a => unions[i].add(a))
-  unions = unions.map(u => u === old ? unions[i] : u)
+for (let [i, [u1, u2]] of sortedPairs.entries()) {
+  unions[u1].forEach(u => unions[u2].add(u))
+  unions[u1].forEach(u => unions[u] = unions[u2])
 
-  if (k == 1000) {
+  if (i == 1000)
     console.log(
       [...new Set(unions)]
         .map(u => u.size)
-        .sort((a,b) => a-b)
+        .sort((s1, s2) => s1-s2)
         .slice(-3)
-        .reduce((a,b) => a*b))
-  }
-  if (unions[0].size == 1000) {
-    console.log(coords[i][0] * coords[j][0])
-    break
-  }
+        .reduce((s1, s2) => s1*s2))
+
+  if (unions[0].size == 1000)
+    process.exit(console.log(coords[u1][0] * coords[u2][0]))
 }
