c7f441c6ca7dd1b1c61dec75aa9f443fd3f2a46c
LiquidFun <brutenis@gmail.com>
2025-12-12 01:11:23 +0100

    Add day 11

--- 2025/11/11.js
+++ 2025/11/11.js
@@ -1,76 +1,31 @@
 let lines = require("fs").readFileSync(0, "utf8").trim().split("\n")
-
-let map = new Map()
-for (let line of lines) {
-  let [from, ...to] = line.split(/:? /)
-  map.set(from, to)
-}
-
-console.log(map.get("you"))
-//console.log(map)
-
-let s1 = 0
+let adj = new Map(lines
+  .map(line => line.split(": "))
+  .map(([from, to]) => [from, to.split(" ")]))
+adj.set("out", [])
 
 function descendants(curr, visited = new Set()) {
   if (visited.has(curr)) return
   visited.add(curr)
-  map.get(curr)?.forEach(next => descendants(next, visited))
+  adj.get(curr).forEach(next => descendants(next, visited))
   return visited
 }
 
 function ancestors(curr) {
-  return new Set(map.keys().filter(node => descendants(node).has(curr)))
+  return new Set(adj.keys().filter(node => descendants(node).has(curr)))
 }
 
-function countPaths(start, end, ...alsoIgnore) {
+function countPaths(start, end) {
   let allowed = descendants(start).intersection(ancestors(end))
-  allowed.add(end)
-  let s = 0
+  let s = 0, head = 0
   let queue = [start]
-  let head = 0
   while (head < queue.length) {
-    if (queue.length > 10_000_000) {
-      queue = queue.slice(head)
-      head = 0
-    }
     let curr = queue[head++]
-    if (curr == end) {
-      s++
-      continue
-    }
-    //if (queue.length%10000 == 0) console.log(queue.length)
-    for (let next of (map.get(curr))) {
-      if (allowed.has(next))
-        queue.push(next)
-    }
+    s += curr == end
+    queue.push(...adj.get(curr).filter(node => allowed.has(node)))
   }
-  console.log(s)
   return s
 }
 
-let svr2fft = countPaths("svr", "fft", "dac")
-//let svr2dac = countPaths("svr", "dac", "fft")
-//let dac2fft = countPaths("dac", "fft")
-let fft2dac = countPaths("fft", "dac")
-//let fft2out = countPaths("fft", "out")
-let dac2out = countPaths("dac", "out")
-
-console.log(svr2fft * fft2dac * dac2out)
-
-
-function countPaths2(start, end) {
-  let queue = [[start]]
-  let head = 0
-  while (head < queue.length) {
-    let [front, ...rest] = queue[head++]
-    if (front == "out") {
-      s1 += rest.includes("dac") && rest.includes("fft")
-      continue
-    }
-    //console.log(front)
-    for (let next of (map.get(front))) {
-      queue.push([next, front, ...rest])
-    }
-  }
-  console.log(s1)
-}
+console.log(countPaths("you", "out"))
+console.log(countPaths("svr", "fft") * countPaths("fft", "dac") * countPaths("dac", "out"))
